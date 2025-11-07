f1a876d52e219a3b9a3b753e4ee20cec08daa94c
shilpagunti <shilpa.gunti@gmail.com>
2025-11-06 19:26:04 -0700

    terraform

---
+++ variables.tf
@@ -0,0 +1,26 @@
+variable "sg_name" {
+    type = string
+}
+
+variable "project_name"{
+    type = string
+}
+
+variable "environment" {
+    type = string
+}
+
+variable "sg_description" {
+    type = string
+    default = "" 
+}
+
+variable "vpc_id" {
+    type = string 
+}
+
+variable "sg_tags" {
+    type = map
+    default = {}
+}
+    
